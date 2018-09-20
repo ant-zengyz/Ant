3268c69723fde6860af35a54d1130781de371a8c
ant-zengyz <2185694954@qq.com>
2018-09-20 13:51:45 +0800

    添加子项目，定时器服务

---
+++ ant-task/src/main/java/com/example/ant/AntTaskApplication.java
@@ -0,0 +1,12 @@
+package com.example.ant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntTaskApplication.class, args);
+    }
+}
